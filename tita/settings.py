2092f66c67d83179fb7da0ec18bb6282b09ec13d
Andrés Camilo Serna <andres.c.serna@gmail.com>
2014-07-24 00:46:08 -0500

    Creación de base.html. Plantilla lista

--- tita/settings.py
+++ tita/settings.py
@@ -79,4 +79,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+# Plantillas de la aplicacion
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR,'templates')
+)
+
+# Archivos estaticos (js, css, template completo, imagenes, etc)
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
