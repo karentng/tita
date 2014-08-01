458538dabb67f720cb2e0b312e60ce1d1188aeba
Andrés Camilo Serna <andres.c.serna@gmail.com>
2014-07-31 19:39:05 -0500

    Interfaz inicial publica

--- convocat/views.py
+++ convocat/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
 
 # Create your views here.
-def base(request):
-    return render(request, 'info.html', {
-        'nombre': 'Nombre'
+
+# vistas de la parte PUBLICA
+def index(request):
+    return render(request, 'publico/index.html', {
+        'opcion_menu': 1
     })
+
+def objetivos(request):
+    return render(request, 'publico/objetivos.html', {
+        'opcion_menu': 2
+    })
+
+def funciones(request):
+    return render(request, 'publico/funciones.html', {
+        'opcion_menu': 3
+    })
+
+def requisitos(request):
+    return render(request, 'publico/requisitos.html', {
+        'opcion_menu': 4
+    })
+
+# falta por hacer
+def registrarse(request):
+    return render(request, 'publico/registrarse.html', {
+        'opcion_menu': 5
+    })
+
+# fin vistas de la parte PUBLICA
--- tita/urls.py
+++ tita/urls.py
@@ -9,5 +9,10 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'convocat.views.base', name="base"),
+    url(r'^$', 'convocat.views.index', name="publico"),
+    url(r'^objetivos/$', 'convocat.views.objetivos', name="publico_objetivos"),
+    url(r'^funciones/$', 'convocat.views.funciones', name="publico_funciones"),
+    url(r'^requisitos/$', 'convocat.views.requisitos', name="publico_requisitos"),
+    url(r'^registrarse/$', 'convocat.views.registrarse', name="publico_registrarse"),
+
 )
