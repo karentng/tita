a4e25a21c499d62263d62b65c012f6a58dcd62b1
Victor Padilla <vector9x@gmail.com>
2014-08-10 11:55:10 -0500

    admin para ver HVs

--- convocat/admin.py
+++ convocat/admin.py
@@ -1,4 +1,57 @@
+from collections import OrderedDict
 from django.contrib import admin
-from convocat.models import AreaEnsenanza
+from convocat.models import *
 # Register your models here.
+
+
+def readonly(self, request, obj=None):
+    if request.user.is_superuser:
+        return self.readonly_fields
+
+    if self.declared_fieldsets:
+        return flatten_fieldsets(self.declared_fieldsets)
+    else:
+        return list(
+            [field.name for field in self.opts.local_fields] +
+            [field.name for field in self.opts.local_many_to_many]
+        )
+
+class AcademicaInline(admin.TabularInline):
+    model = FormacionAcademica
+    get_readonly_fields = readonly
+
+class FormacionTicsAdmin(admin.TabularInline):
+    model = FormacionTics
+    get_readonly_fields = readonly
+
+class ExperienciaEnsenanzaInline(admin.TabularInline):
+    model = ExperienciaEnsenanza
+    get_readonly_fields = readonly
+
+class IdiomaInline(admin.TabularInline):
+    model = Idioma
+    get_readonly_fields = readonly
+
+
+class ExperienciaOtraAdmin(admin.TabularInline):
+    model = ExperienciaOtra
+    get_readonly_fields = readonly
+
+class ConocimientoInline(admin.StackedInline):
+    model = ConocimientosEspecificos
+    get_readonly_fields = readonly
+
+class AspiranteAdmin(admin.ModelAdmin):
+    list_display = ('nombre_completo', 'puntuacion_hv')
+    inlines = [AcademicaInline, FormacionTicsAdmin, ConocimientoInline, IdiomaInline, ExperienciaEnsenanzaInline, ExperienciaOtraAdmin]
+
+    get_readonly_fields = readonly
+
+    def nombre_completo(self, obj):
+        return unicode(obj)
+
+
+
 admin.site.register(AreaEnsenanza)
+admin.site.register(Aspirante, AspiranteAdmin)
+
