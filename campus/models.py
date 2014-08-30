8c970b3acd99296083701077843902eb88f1918d
Karent Nathalia Narvaez <karentnathalia@gmail>
2014-08-30 16:23:51 -0500

    moviendo modelos a aplicaion campus

--- campus/models.py
+++ campus/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from convocat.models import Municipio
 
-# Create your models here.
+
+OPCIONES_GRADO = (  # son enteros, crear campo como IntegerField(choices=OPCIONES_GRADO)
+    ( 1, u'Primero'),
+    ( 2, u'Segundo'),
+    ( 3, u'Tercero'),
+    ( 4, u'Cuarto'),
+    ( 5, u'Quinto'),
+    ( 6, u'Sexto'),
+    ( 7, u'Séptimo'),
+    ( 8, u'Octavo'),
+    ( 9, u'Noveno'),
+    (10, u'Décimo'),
+    (11, u'Once'),
+)
+
+class InstitucionEducativa(Model):
+    municipio = ForeignKey(Municipio)
+    nombre = CharField(max_length=200)
--- estudiante/models.py
+++ estudiante/models.py
@@ -1,22 +1,4 @@
 #encoding: utf-8
 from django.db.models import *
-from convocat.models import Municipio
+
 # Create your models here.
-
-OPCIONES_GRADO = (  # son enteros, crear campo como IntegerField(choices=OPCIONES_GRADO)
-    ( 1, u'Primero'),
-    ( 2, u'Segundo'),
-    ( 3, u'Tercero'),
-    ( 4, u'Cuarto'),
-    ( 5, u'Quinto'),
-    ( 6, u'Sexto'),
-    ( 7, u'Séptimo'),
-    ( 8, u'Octavo'),
-    ( 9, u'Noveno'),
-    (10, u'Décimo'),
-    (11, u'Once'),
-)
-
-class InstitucionEducativa(Model):
-    municipio = ForeignKey(Municipio)
-    nombre = CharField(max_length=200)
