fd1c62456578ff40b3e9a3e2924628523a776c8d
Andrés Camilo Serna <andres.c.serna@gmail.com>
2014-08-31 22:47:44 -0500

    formulario inscripcion DE

--- campus/models.py
+++ campus/models.py
@@ -56,17 +56,19 @@ GRADOS = (
 )
 
 JORNADAS = (
+    ('C', 'Completa'),
     ('M', 'Mañana'),
     ('T', 'Tarde'),
-    ('A', 'Mañana y Tarde')
+    ('N', 'Nocturna')
 )
 
 class InstitucionEducativa(models.Model):
-    municipio = models.ForeignKey(Municipio)
-    nombre = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=100)
+    rector = models.CharField(max_length=100, null=True)
+    principal = models.ForeignKey("self", null=True)
 
     def __unicode__(self):
-        return self.nombre
+        return (u"%s"%self.nombre)
 
 class Cargo(models.Model):
     nombre = models.CharField(max_length=200)
@@ -106,30 +108,115 @@ class Curso(models.Model):
     def __unicode__(self):
         return self.descripcion
 
-#Corresponde a los Maestros Estudiantes    
+
+
+#Horarios disponibles
+DIAS=(
+    (1, 'Lunes'),
+    (2, 'Martes'),
+    (3, 'Miércoles'),
+    (4, 'Jueves'),
+    (5, 'Viernes')
+)
+class HorarioClase(models.Model):
+    dia = models.IntegerField(choices=DIAS, verbose_name="dia de clase")
+    inicio = models.TimeField(auto_now=False)
+    fin = models.TimeField(auto_now=False)
+    grado = models.ForeignKey(Grado)
+
+#Corresponde a los Maestros Estudiantes 
 class Estudiante(models.Model):
+    
+    NIVEL_EDUCATIVO = (
+        (1, 'Sin título'),
+        (2, 'Bachiller académico con profundización en pedagogía'),
+        (3, 'Técnico o tecnólogo en educación'),
+        (4, 'Técnico o tecnólogo en otras áreas'),
+        (5, 'Profesional o licenciado en educación'),
+        (6, 'Profesional en otras áreas, no licenciado'),
+        (7, 'Postgrado en educación'),
+        (8, 'Postgrado en otras áreas')
+    )
+
     numero_documento = models.BigIntegerField(unique=True, verbose_name='número documento')
     nombre1 = models.CharField( max_length=255, verbose_name='primer nombre')
     nombre2 = models.CharField( max_length=255, blank=True, verbose_name='segundo nombre')
     apellido1 = models.CharField( max_length=255, verbose_name='primer apellido')
     apellido2 = models.CharField( max_length=255, blank=True, verbose_name='segundo apellido')
-    sexo = models.CharField( choices=[('M','Hombre'), ('F', 'Mujer')], max_length=1, verbose_name='sexo')
-    
+    sexo = models.CharField( choices=[('M','Hombre'), ('F', 'Mujer')], max_length=1, verbose_name='genero')
+    email = models.EmailField()
+    email_institucional = models.EmailField()
+    municipio = models.ForeignKey(Municipio, verbose_name='municipio de residencia', null=True)
     telefono = models.BigIntegerField(null=True, blank=True, verbose_name=u'teléfono fijo')
     celular = models.BigIntegerField(null=True, blank=True, verbose_name=u'número de celular')
-    email = models.EmailField()
-    
+    direccion = models.CharField(verbose_name="dirección", max_length=100)
+    nivel_educativo = models.IntegerField(choices=NIVEL_EDUCATIVO, verbose_name="último nivel educativo aprobado")
+
     formador = models.ForeignKey(Formador, verbose_name='formador', null=True)
     curso = models.ForeignKey(Curso, verbose_name='curso', null=True)
 
-    instituciones = models.ManyToManyField(InstitucionEducativa)
-    cargos = models.ManyToManyField(Cargo)
-    asignaturas = models.ManyToManyField(Asignatura)
-    grados = models.ManyToManyField(Grado)
-    jornada = models.CharField(choices=JORNADAS, max_length=1)
-
+    aprobo = models.NullBooleanField()
+    
     def __unicode__(self):
         return (u"%s %s %s %s"%(self.nombre1,self.nombre2 or '', self.apellido1, self.apellido2 or '')).strip() or "-"
+
+class SecretariasEducacion(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return (u"%s"%self.nombre)
+
+class CertificacionTIC(models.Model):
+    estudiante = models.ForeignKey(Estudiante)
+    nombre = models.CharField(max_length=100, verbose_name="nombre de la certificación")
+    entidad = models.CharField(max_length=100, verbose_name="entidad certificadora")
+    fecha = models.DateTimeField(verbose_name="fecha de la certificación")
+
+    def __unicode__(self):
+        return (u"%s"%self.nombre)
+
+class ProgramasTIC(models.Model):
+    estudiante = models.ForeignKey(Estudiante)
+    nombre = models.CharField(max_length=100, verbose_name="nombre del programa")
+    fecha = models.DateTimeField(verbose_name="fecha de la participación en el programa")
+
+    def __unicode__(self):
+        return (u"%s"%self.nombre)
+
+class DatosLaborales(models.Model):
+
+    estudiante = models.ForeignKey(Estudiante)
+
+    secretaria_educacion = models.ForeignKey(SecretariasEducacion)
+    institucion_educativa = models.ForeignKey(InstitucionEducativa)
+    cargos = models.IntegerField(choices=[(1, 'Docente'), (2, 'Directivo Docente')], verbose_name="cargo")
+    sector = models.CharField( choices=[('O','Oficial'), ('N', 'No Oficial')], max_length=1, verbose_name='sector')
+    zona = models.CharField( choices=[('R','Rural'), ('U', 'Urbana'), ('N', 'N.A')], max_length=1, verbose_name='zona')
+    jornada = models.CharField(choices=JORNADAS, max_length=1)
+
+    grados = models.ManyToManyField(Grado)
+    asignaturas = models.ManyToManyField(Asignatura)
+    decreto_docente = models.IntegerField( choices=[(1,'D.L 1278 de 2002'), (2, 'D.L 2277 de 1979')], max_length=1, verbose_name='decreto profesional docente')
+    #grado escalafon
+    nombramiento = models.IntegerField(choices=[(1,'Propiedad'), (2, 'Período de Prueba'), (3, 'Provisional')], max_length=1, verbose_name='tipo de nombramiento')
+
+    etnoeducador = models.BooleanField(verbose_name="se desempeña como etnoeducador")
+    tipo_etnoeducador = models.IntegerField(choices=[(1, 'Raizal'),(2, 'Afrocolombiano'),(3, 'Indígena'), (4, 'N.A')], null=True)
+    poblacion_etnica = models.CharField(max_length="100", verbose_name="poblacion étnica que atiende", null=True)
+    
+class Horario(models.Model):
+    DIAS = (
+        (1, 'Lunes'),
+        (2, 'Martes'),
+        (3, 'Miércoles'),
+        (4, 'Jueves'),
+        (5, 'Viernes')
+    )
+    estudiante = models.ForeignKey(Estudiante)
+    dia = models.IntegerField(choices=DIAS, verbose_name="día de la semana")
+    inicio = models.TimeField(verbose_name="hora inicial")
+    fin = models.TimeField(verbose_name="hora final")
+    curso = models.ForeignKey(Asignatura)
 
 class Clase(models.Model):
     fecha_programada = models.DateTimeField(verbose_name=u'fecha de realización')
