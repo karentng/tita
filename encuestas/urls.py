a8e0b3c638a4bbd88d6a318850f9cf4caae7034a
Victor Padilla <vector9x@gmail.com>
2014-09-03 15:46:30 -0500

    borrando app encuestas vieja

--- encuestas/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django.views.generic.base import TemplateView
-from django.contrib import admin
-from django.conf import settings
-
-urlpatterns = patterns('encuestas.views',
-    url(r'padres$',   'encuesta_padre', name="encuesta_padre"),
-)
