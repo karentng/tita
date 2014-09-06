1e2be808c3e9b53f17c6f9b4238f31a8e137245c
Luisa Bedoya <luisabedoy@gmail.com>
2014-09-06 13:08:45 -0500

    form views cronograma

--- cronograma/forms.py
+++ cronograma/forms.py
@@ -3,9 +3,12 @@ from django import forms
 from cronograma.models import Evento
 from datetimewidget.widgets import DateWidget
 
+def MyDateWidget():
+    return DateWidget(usel10n=False, bootstrap_version=3, options={'format': 'yyyy-mm-dd', 'startView':4, 'language':'es'})
+
 class EventoForm(forms.ModelForm):
     class Meta:
-        model = Evento
+        model = Evento 
         fields = ('nombre', 'fecha_inicio', 'fecha_finalizacion','descripcion')
         widgets = {
             #'fecha_inicio': DateTimePicker(options={'format':'YYYY-MM-DD',  'pickTime':False}),
