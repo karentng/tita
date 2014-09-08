8e5b293c90278aca127c3f144746d779512bd26e
Andrés Camilo Serna <andres.c.serna@gmail.com>
2014-09-07 20:34:19 -0500

    cronograma mostrando eventos programados

--- cronograma/views.py
+++ cronograma/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from cronograma.forms import *
-
+from cronograma.models import Evento
+import json
 
 def cronograma(request):
     # if this is a POST request we need to process the form data
@@ -13,8 +14,19 @@ def cronograma(request):
         evento = EventoForm()
 
     eventos = Evento.objects.all()
+    events = []
 
+    for i in eventos:
+        inicio = i.fecha_inicio
+        fin = i.fecha_finalizacion
+        events.append({
+            'nombre': i.nombre,
+            'inicio': [inicio.year, inicio.month, inicio.day],
+            'fin': [fin.year, fin.month, fin.day],
+            'descripcion': i.descripcion
+        })
+    
     return render(request, 'cronograma.html', {
         'evento': evento,
-        'eventos': eventos
+        'eventos': json.dumps(events)
     })
