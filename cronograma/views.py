d92f4754ad266af644492c9d05decdc1038afe56
Andrés Camilo Serna <andres.c.serna@gmail.com>
2014-09-16 23:07:11 -0500

    cronogramas visualizar por hora

--- cronograma/views.py
+++ cronograma/views.py
@@ -3,6 +3,8 @@ from cronograma.forms import EventosAcompanamientoForm, EventosDiplomadoForm
 from cronograma.models import EventosAcompanamiento, EventosDiplomado
 import json
 from django.shortcuts import redirect, render, render_to_response
+from datetime import datetime, date, timedelta
+from math import ceil
 
 def cronograma(request):
     # if this is a POST request we need to process the form data
@@ -16,7 +18,29 @@ def cronograma(request):
 
     eventos = EventosAcompanamiento.objects.all()
     events = []
+    for i in eventos:
+        inicio = i.fecha_inicio
+        fin = i.fecha_finalizacion
 
+        hora_inicio = [i.hora_inicio.hour, i.hora_inicio.minute, 0]
+        hora_finalizacion = [i.hora_finalizacion.hour, i.hora_finalizacion.minute, 0]
+
+        diasTotal = (fin - inicio).days
+        
+        diasEvento = []
+        for j in range(0, diasTotal+1):
+            aux = inicio + timedelta(days = j)
+            diasEvento.append([aux.year, aux.month-1, aux.day])
+
+        events.append({
+            'nombre': i.nombre,
+            'descripcion': i.descripcion,
+            'hora_inicio': hora_inicio,
+            'hora_finalizacion': hora_finalizacion,
+            'diasEvento': diasEvento,
+            'institucion': i.get_institucion_display()
+        })
+    '''
     for i in eventos:
         inicio = i.fecha_inicio
         fin = i.fecha_finalizacion
@@ -25,12 +49,17 @@ def cronograma(request):
             'inicio': [inicio.year, inicio.month-1, inicio.day],
             'fin': [fin.year, fin.month-1, fin.day],
             'descripcion': i.descripcion
-        })
+        })'''
     
     return render(request, 'cronograma.html', {
         'formAcompanamiento': form,
         'eventos': json.dumps(events)
     })
+
+def menor10(val):
+    if val < 10:
+        return "0"+str(val)
+    return str(val)
 
 def diplomado(request):
     # if this is a POST request we need to process the form data
@@ -48,11 +77,23 @@ def diplomado(request):
     for i in eventos:
         inicio = i.fecha_inicio
         fin = i.fecha_finalizacion
+
+        hora_inicio = [i.hora_inicio.hour, i.hora_inicio.minute, 0]
+        hora_finalizacion = [i.hora_finalizacion.hour, i.hora_finalizacion.minute, 0]
+
+        diasTotal = (fin - inicio).days
+        
+        diasEvento = []
+        for j in range(0, diasTotal+1):
+            aux = inicio + timedelta(days = j)
+            diasEvento.append([aux.year, aux.month-1, aux.day])
+
         events.append({
             'nombre': i.nombre,
-            'inicio': [inicio.year, inicio.month-1, inicio.day],
-            'fin': [fin.year, fin.month-1, fin.day],
-            'descripcion': i.descripcion
+            'descripcion': i.descripcion,
+            'hora_inicio': hora_inicio,
+            'hora_finalizacion': hora_finalizacion,
+            'diasEvento': diasEvento
         })
     
     return render(request, 'diplomado.html', {
