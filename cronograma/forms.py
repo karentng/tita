1274cf94865bce26cb3d6da798e79a9855638d16
Luisa Bedoya <luisabedoy@gmail.com>
2014-09-21 14:30:33 -0500

    cambio widget DateWidget por DateTimeWidget en cronograma

--- cronograma/forms.py
+++ cronograma/forms.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from campus.models import Clase
-from convocat.forms import MyDateWidget
 from datetimewidget.widgets import DateTimeWidget
 
 
@@ -13,12 +12,9 @@ class EventosDiplomadoForm(forms.ModelForm):
         fields = ('nombre', 'fecha_inicio', 'fecha_finalizacion', 'descripcion')
 
         widgets = {
-            'fecha_inicio': MyDateWidget(),
-            'fecha_finalizacion': MyDateWidget(),
-         	#'hora_inicio' : forms.TimeInput(format='%H:%M'),
-            'hora_inicio': DateTimeWidget(options={'format': 'hh:ii','viewSelect':'hour','autoclose': 'true', 'showMeridian' : 'false', 'startView':'1', 'minview':'0','maxView':'1','minuteStep':'30','keyboardNavigation':'false',},usel10n=False, bootstrap_version=3),
-            'hora_finalizacion': DateTimeWidget(options={'format': 'hh:ii','viewSelect':'hour','autoclose': 'true', 'showMeridian' : 'false', 'startView':'1', 'minview':'0','maxView':'1','minuteStep':'30','keyboardNavigation':'false',},usel10n=False, bootstrap_version=3),
-            'descripcion': forms.Textarea(attrs={'rows': 3})
+            'fecha_inicio': DateTimeWidget(options={'format': 'dd/mm/yyyy hh:ii','autoclose': 'true','startView':'4','minuteStep':'30',},usel10n=False, bootstrap_version=3),
+            'fecha_finalizacion': DateTimeWidget(options={'format': 'dd/mm/yyyy hh:ii','autoclose': 'true','startView':'4','minuteStep':'30',},usel10n=False, bootstrap_version=3),
+         	'descripcion': forms.Textarea(attrs={'rows': 4})
         }
 
 class EventosAcompanamientoForm(forms.ModelForm):
@@ -27,11 +23,7 @@ class EventosAcompanamientoForm(forms.ModelForm):
 
         fields = ('institucion', 'nombre', 'fecha_inicio','fecha_finalizacion', 'descripcion')
         widgets = {
-            'fecha_inicio': MyDateWidget(),
-            'fecha_finalizacion': MyDateWidget(),
-         	#'hora_inicio' : forms.TimeInput(format='%H:%M'),
-            #'hora_inicio' : DateWidget(usel10n=False, bootstrap_version=3, options={'format': 'hh:mm'}),
-            'hora_inicio': DateTimeWidget(options={'format':'hh:ii','viewSelect':'hour','autoclose': 'true', 'showMeridian' : 'false', 'startView':'1', 'minview':'0','maxView':'1','minuteStep':'30','keyboardNavigation':'false',},usel10n=False, bootstrap_version=3),
-            'hora_finalizacion': DateTimeWidget(options={'format':'hh:ii','viewSelect':'hour','autoclose': 'true', 'showMeridian' : 'false', 'startView':'1', 'minview':'0','maxView':'1','minuteStep':'30','keyboardNavigation':'false',},usel10n=False, bootstrap_version=3),
-            'descripcion': forms.Textarea(attrs={'rows': 3})
+            'fecha_inicio': DateTimeWidget(options={'format': 'dd/mm/yyyy hh:ii','autoclose': 'true','startView':'4','minuteStep':'30',},usel10n=False, bootstrap_version=3),
+            'fecha_finalizacion': DateTimeWidget(options={'format': 'dd/mm/yyyy hh:ii','autoclose': 'true','startView':'4','minuteStep':'30',},usel10n=False, bootstrap_version=3),
+            'descripcion': forms.Textarea(attrs={'rows': 4})
         }
