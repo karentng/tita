17468cad54e8e9cff3e4524c135ae8a56623620c
Karent Nathalia Narvaez <karentnathalia@gmail>
2014-11-13 18:05:56 -0500

    nueva encuesta maestros

--- survey/urls.py
+++ survey/urls.py
@@ -17,6 +17,7 @@ urlpatterns = patterns('',
     url(r'docente$', 'survey.views.encuesta_docente', name='encuesta_docente'),
     url(r'estudiante$', 'survey.views.encuesta_estudiante', name='encuesta_estudiante'),
     url(r'nuevoEstudiante$', 'survey.views.encuesta_nuevo_estudiante', name='encuesta_nuevo_estudiante'),
+    url(r'nuevoMaestro$', 'survey.views.encuesta_nuevo_maestro', name='encuesta_nuevo_maestro'),
 
     url(r'finalizada$', TemplateView.as_view(template_name='finalizada.html'), name="encuesta_finalizada"),
 
