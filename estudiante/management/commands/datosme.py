ba00bb39d1284deb62b09ebfe34c70ba36cbcde5
Andrés Camilo Serna <andres.c.serna@gmail.com>
2014-12-14 21:10:51 -0500

    CSV ME con tildes correctamente

--- estudiante/management/commands/datosme.py
+++ estudiante/management/commands/datosme.py
@@ -10,12 +10,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         archivo = open("MaestrosEstudiante.csv","w") 
         writer = csv.writer(archivo, delimiter=';')
-        writer.writerow(["Grupos", "Nombre Completo", u"Cédula".encode('iso-8859-1'),
-            u"Género".encode('iso-8859-1'), u"Correo Personal".encode('iso-8859-1'), "Correo Institucional", "Municipio",
-            u"Teléfono Fijo".encode('iso-8859-1'), "Celular", u"Dirección".encode('iso-8859-1'), "Nivel Educativo",
-            u"Secretaría de Educación".encode('iso-8859-1'),"Sede", "Cargo", "Zona", "Jornada", "Asignaturas",
+        writer.writerow(["Grupos", "Nombre Completo", u"Cédula".encode('latin-1'),
+            u"Género".encode('latin-1'), u"Correo Personal".encode('latin-1'), "Correo Institucional", "Municipio",
+            u"Teléfono Fijo".encode('latin-1'), "Celular", u"Dirección".encode('latin-1'), "Nivel Educativo",
+            u"Secretaría de Educación".encode('latin-1'),"Sede", "Cargo", "Zona", "Jornada", "Asignaturas",
             "Grados", "Decreto Docente", "Nombramiento", "Tipo Etnoeducador",
-            u"Formación Académica".encode('iso-8859-1'), "Certificaciones TIC"])
+            u"Formación Académica".encode('latin-1'), "Certificaciones TIC"])
 
         aspirantes = Estudiante.objects.all()
         for aspirante in aspirantes:
@@ -30,30 +30,30 @@ class Command(BaseCommand):
             num_doc = aspirante.numero_documento
             municipio = Municipio.objects.get(id=aspirante.municipio_id)
 
-            sede = infoLaboral.get_sede_display().encode('iso-8859-1')
-            cargo = infoLaboral.get_cargo_display().encode('iso-8859-1')
-            zona = infoLaboral.get_zona_display().encode('iso-8859-1')
-            jornada = infoLaboral.get_jornada_display().encode('iso-8859-1')
+            sede = infoLaboral.get_sede_display().encode('latin-1')
+            cargo = infoLaboral.get_cargo_display().encode('latin-1')
+            zona = infoLaboral.get_zona_display().encode('latin-1')
+            jornada = infoLaboral.get_jornada_display().encode('latin-1')
 
             asigs = infoLaboral.asignaturas.all()
             asignaturas = ""
             for i in asigs:
-                asignaturas+= i.nombre.encode('iso-8859-1')+" - "
+                asignaturas+= i.nombre.encode('latin-1')+" - "
 
             grads = infoLaboral.grados.all()
             grados = ""
             for i in grads:
-                grados+= i.nombre.encode('iso-8859-1')+" - "
+                grados+= i.nombre.encode('latin-1')+" - "
 
             formaciones = ""
             for i in formacion:
-                formaciones+= "["+i.titulo.encode('iso-8859-1')+" - "+i.get_nivel_display().encode('iso-8859-1')+" - "+i.institucion.encode('iso-8859-1')+"]"
+                formaciones+= "["+i.titulo.encode('latin-1')+" - "+i.get_nivel_display().encode('latin-1')+" - "+i.institucion.encode('latin-1')+"]"
 
             certificaciones = ""
             for i in certificacion:
-                certificaciones+= "["+i.nombre.encode('iso-8859-1')+" - "+i.get_duracion_display().encode('iso-8859-1')+" - "+i.entidad.encode('iso-8859-1')+"]"
+                certificaciones+= "["+i.nombre.encode('latin-1')+" - "+i.get_duracion_display().encode('latin-1')+" - "+i.entidad.encode('latin-1')+"]"
 
-            decreto_docente = infoLaboral.get_decreto_docente_display().encode('iso-8859-1')
+            decreto_docente = infoLaboral.get_decreto_docente_display().encode('latin-1')
             if infoLaboral.decreto_docente < 15:
                 decreto_docente+= " - "+"D.L 2277 de 1979"
             else:
@@ -62,13 +62,13 @@ class Command(BaseCommand):
             curs = Cursos.objects.filter(estudiantes=aspirante)
             cursos = ""
             for i in curs:
-                cursos+= i.descripcion.encode('iso-8859-1')+" - "
+                cursos+= i.descripcion.encode('latin-1')+" - "
             
             arreglo = [cursos, nombre, num_doc, aspirante.sexo, aspirante.email, aspirante.email_institucional,
-            municipio.nombre.encode('iso-8859-1'), aspirante.telefono, aspirante.celular,
-            aspirante.direccion.encode('iso-8859-1'), aspirante.get_nivel_educativo_display().encode('iso-8859-1'),
+            municipio.nombre.encode('latin-1'), aspirante.telefono, aspirante.celular,
+            aspirante.direccion.encode('latin-1'), aspirante.get_nivel_educativo_display().encode('latin-1'),
             infoLaboral.secretaria_educacion.nombre,
             sede, cargo, zona, jornada, asignaturas, grados, decreto_docente,
-            infoLaboral.get_nombramiento_display().encode('iso-8859-1'),
-            infoLaboral.get_tipo_etnoeducador_display().encode('iso-8859-1'), formaciones, certificaciones]
+            infoLaboral.get_nombramiento_display().encode('latin-1'),
+            infoLaboral.get_tipo_etnoeducador_display().encode('latin-1'), formaciones, certificaciones]
             writer.writerow(arreglo)
