81e78711e16ec1895508b9bd463294b4e8e36cdf
Karent Nathalia Narvaez <karentnathalia@gmail>
2014-12-19 13:50:19 -0500

    optimizacion reporte encuestas

--- survey/management/commands/reportesencuestas.py
+++ survey/management/commands/reportesencuestas.py
@@ -3,6 +3,7 @@ from django.core.management.base import BaseCommand, CommandError
 import csv
 from django.http import HttpResponse
 from survey.models import *
+from django.db import connection
 
 
 class Command(BaseCommand):
@@ -10,19 +11,24 @@ class Command(BaseCommand):
         survey_id = args[0]
 
         respuestas = Response.objects.filter(survey_id=survey_id)                       
-        archivo = open("salida.csv","w") 
-        writer = csv.writer(archivo)
-
+        archivo = open("reporte_encuesta.csv","w") 
+        writer = csv.writer(archivo, delimiter=';')
+        contador = 0
+        #print "nro respuestas", len(respuestas)
+        #print "nro queries antes de iniciar", len(connection.queries)
         for respuesta in respuestas: 
-            item = respuesta.id, respuesta.created, respuesta.updated,          
-
-            respuestas_radio = AnswerRadio.objects.filter(response_id=respuesta.id)
-            respuestas_select = AnswerSelect.objects.filter(response_id=respuesta.id)
-            respuestas_select_multiple = AnswerSelectMultiple.objects.filter(response_id=respuesta.id)
-            respuestas_text = AnswerText.objects.filter(response_id=respuesta.id)
-
+            item = [respuesta.id, respuesta.created]        
+            #print "oso", len(connection.queries)
+            respuestas_radio = AnswerRadio.objects.filter(response_id=respuesta.id).select_related('question')
+            respuestas_select = AnswerSelect.objects.filter(response_id=respuesta.id).select_related('question')
+            respuestas_select_multiple = AnswerSelectMultiple.objects.filter(response_id=respuesta.id).select_related('question')
+            respuestas_text = AnswerText.objects.filter(response_id=respuesta.id).select_related('question')
+            
+             
             respuestas_todas = list(respuestas_radio)+list(respuestas_select)+list(respuestas_select_multiple)+list(respuestas_text)
+            #print "pinteta", len(connection.queries)
             respuestas_todas.sort(key = lambda res : res.question.number)
+            #print "emico", len(connection.queries)
 
             for respuesta_todas in respuestas_todas:            
                 if isinstance(respuesta_todas, AnswerSelectMultiple):
@@ -30,9 +36,15 @@ class Command(BaseCommand):
                 else :
                     valor = respuesta_todas.body
                 
-                item += (valor.encode('iso-8859-1'),)
+                item.append(valor.encode('iso-8859-1'))
+            #print "osopeteno", len(connection.queries)
 
             writer.writerow(item)
+
+            #print len(connection.queries)
+            #raw_input('enter para continuar')
+            #if True or contador%100==0:
+            #    print len(connection.queries)
 
         #unicode(book.author).encode('iso-8859-1')
 
--- survey/views.py
+++ survey/views.py
@@ -1,11 +1,14 @@
 # encoding: utf-8
 from django.shortcuts import render, redirect
+import csv
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.core import urlresolvers
 from django.contrib import messages
 import datetime
+from survey.models import *
+
 #import settings
 
 from models import Question, Survey, Category
@@ -248,3 +251,37 @@ def encuesta_nuevo_padre(request):
         'camposHerramientas1': camposHerramientas[:7],
         'camposHerramientas2': camposHerramientas[7:],
     })
+
+def reporte_encuesta(request, survey_id):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="reporte_encuesta.csv"'
+    writer = csv.writer(response, delimiter=";")
+    print "encuesta numero "+survey_id
+    if survey_id==4:
+        print "entro"
+        writer.writerow(['ID','Fecha de Encuesta','Jornada','Nombre de tu institución educativa','Sus nombre(s) y apellidos:','¿Qué materias enseña?','Grados en los que enseña:', 'Edad:','Fecha de Nacimiento','Lugar de nacimiento:','Nombre del barrio donde reside actualmente:','¿Cuál es su máximo nivel educativo? Marque con una X su respuesta.','Título obtenido:','¿A qué área de formación corresponde su carrera de pregrado? Marque con una X su respuesta.','Otra. ¿Cuál?','¿Cuántos años lleva ejerciendo la docencia? Marque con una X su respuesta.','1. Actualmente ¿con qué frecuencia utiliza el computador? Marque con una X sólo una respuesta.','2. Actualmente ¿con qué frecuencia se conecta a Internet? Marque con una X sólo una respuesta.','3. ¿Qué opina sobre que sus estudiantes hagan las tareas con ayuda de Internet? Marque con una X sólo una respuesta.','4. Califique de 1 a 3 las materias que usted enseña según qué tanto le serviría utilizar Internet en cada una de ellas. Si cree que le serviría mucho marque 3, si cree que le serviría un poco marque 2 y si cree que le serviría muy poquito o nada marque 1.','Calificación','Nombre materia 2','Calificación','Nombre materia 3','Calificación','Nombre materia 4','Calificación','Nombre materia 5','Calificación','5. Frente al uso del computador y la Internet en mi trabajo… Marque con una X sólo una respuesta.','6. El computador y la Internet dispersan la atención de los estudiantes.','7. El computador y la Internet limitan la capacidad de creación de los estudiantes.','8. La Internet está perjudicando las capacidades de lectura y escritura de los estudiantes.','9. Justifique su respuesta a la pregunta anterior (No. 8). En esta pregunta debe explicar las razones por las que escogió una de las tres opciones de la pregunta 8.','10. ¿Le genera dificultades el uso del computador y la Internet? Marque con una X su respuesta.','11. Mencione las dificultades que tiene cuando usa computador e Internet:','12. ¿Usted se ha certificado en algún curso de formación en TIC? Marque con una X su respuesta.','¿Cuáles?','Si usted no se ha certificado en algún curso de formación en TIC, pero actualmente está realizando un curso de este tipo, mencione ¿Cuál(es)?','Procesador de texto (Apache OpenOffice Writer, LibreOffice Writer).','Bases de datos (MySQL, ORACLE, Microsoft Access).','Correo electrónico.','Hoja de cálculo (Microsoft Excel, Apache Open Office Calc, Spreadsheet de Google Drive).','Programas para hacer presentaciones (Microsoft Power Point, Prezi).','Programas de dibujo (Corel Draw, Adobe Flash, Microsoft Paint). ','Programas para editar imágenes y fotografías (Adobe Photoshop, GIMP)','Internet para realizar consultas generales.','Medios audiovisuales.','Internet para hacer actividades pedagógicas.','Plataforma virtual de aprendizaje.','Blog personal para interactuar con sus alumnos.','Sitios especializados en temas que le interesan.','Software especializado.','Utiliza el computador y la Internet para el diseño de proyectos de aula.','Diseña y emplea ambientes constructivos, críticos, reflexivos y colaborativos, apoyándose en el computador y la Internet.','Otra herramienta. ¿Cuál?','14. ¿En qué sitios se conecta a Internet? Marque con una X sus respuestas. Puede marcar varias opciones. ','15. En el último mes de trabajo ¿ha utilizado el computador en sus clases? Marque con una X su respuesta. ','16. ¿Qué tareas o trabajos les pone a hacer a sus estudiantes en Internet? Marque con una X sus respuestas.Puede marcar varias opciones. ','Otra actividad. ¿Cuál?','17. Si usted pertenece a algún grupo o red virtual en la que se aborden temas sobre el uso del computador y la Internet en la enseñanza y el aprendizaje, mencione en cuál(es):','18. ¿Qué actividades relacionadas con el uso del computador y la Internet en la enseñanza y el aprendizaje hace con otros profesores? ','19. ¿De qué manera usted investiga en Internet sobre un tema que le interesa para sus clases? Marque con una X su respuesta. Puede marcar varias opciones. ','Otra forma. ¿Cuál?','Computador','Lo uso para otra cosa. Mencione ¿cuál?','Internet','Lo uso para otra cosa. Mencione ¿cuál?','Tablet','Lo uso para otra cosa. Mencione ¿cuál?','Celular','Lo uso para otra cosa. Mencione ¿cuál?','Videojuegos','Lo uso para otra cosa. Mencione ¿cuál?','Cámara fotográfica digital','Lo uso para otra cosa. Mencione ¿cuál?','Cámara de video','Lo uso para otra cosa. Mencione ¿cuál?','Grabadora digital','Lo uso para otra cosa. Mencione ¿cuál?','DVD/Blue-ray','Lo uso para otra cosa. Mencione ¿cuál?','Televisor','Lo uso para otra cosa. Mencione ¿cuál?','Radio','Lo uso para otra cosa. Mencione ¿cuál?','Otro ','21. ¿A qué redes sociales pertenece? Marque con una X sus respuestas. Puede marcar varias opciones. ','Otra. ¿Cuál?','22. ¿Con quién comparte la información que encuentra en Internet? Marque con una X sus respuestas. Puede marcar varias opciones. ','23. ¿Con quién aprende a usar el computador y la Internet? Marque con una X sus respuestas. Puede marcar varias opciones.''Haciendo cursos. ¿Cuáles?','24. Cuando usa Internet ¿con quién prefiere hacerlo? Marque con una X sólo una respuesta.','25. ¿Qué le gustaría hacer o aprender a hacer con Internet? Marque con una X sus respuestas. Puede marcar varias opciones','Otra cosa. ¿Cuál?','Primer dispositivo que lamentaría','Segundo dispositivo que lamentaría','Tercer dispositivo que lamentaría','27. Suponiendo que el escenario descrito al inicio es real', '¿qué cree que sucedería con el desempeño y el comportamiento de sus estudiantes en clase? Marque con una X sólo una respuesta.','28. Suponiendo que el escenario descrito al inicio es real, ¿qué cree que sucedería con las tareas y responsabilidades de los profesores con los nuevos equipos? Marque con una X sólo una respuesta.','29. Suponiendo que el escenario descrito al inicio es real, ¿qué cree que sucedería con la comunicación entre los padres de familia y la institución educativa? Marque con una X sólo una respuesta.','30. ¿Le gustó responder esta encuesta en computador? Marque con una X su respuesta.'])
+
+
+    respuestas = Response.objects.filter(survey_id=survey_id)
+
+    for respuesta in respuestas: 
+        item = respuesta.id, respuesta.created,         
+
+        respuestas_radio = AnswerRadio.objects.filter(response_id=respuesta.id).select_related('question')
+        respuestas_select = AnswerSelect.objects.filter(response_id=respuesta.id).select_related('question')
+        respuestas_select_multiple = AnswerSelectMultiple.objects.filter(response_id=respuesta.id).select_related('question')
+        respuestas_text = AnswerText.objects.filter(response_id=respuesta.id).select_related('question')
+
+        respuestas_todas = list(respuestas_radio)+list(respuestas_select)+list(respuestas_select_multiple)+list(respuestas_text)
+        respuestas_todas.sort(key = lambda res : res.question.number)
+
+        for respuesta_todas in respuestas_todas:            
+            if isinstance(respuesta_todas, AnswerSelectMultiple):
+                valor =  "|".join(eval(respuesta_todas.body))
+            else :
+                valor = respuesta_todas.body
+            
+            item += (valor.encode('iso-8859-1'),)
+
+        writer.writerow(item)
+    return response
